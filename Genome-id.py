8bcc8f1b9b4dc47ff570939a109a19faba53b887
Shreya Singh <ssinghshreya.28@gmail.com>
2025-04-10 10:51:27 +0530

    Update Genome-id.py

--- Genome-id.py
+++ Genome-id.py
@@ -1,4 +1,11 @@
-df=pd.read_csv('dataa.csv',encoding='latin1')
-df
+import pandas as pd
+import numpy as np
+ 
+df = pd.read_csv('data.csv', encoding='latin1')
+
+# Group by 'Genome Name' and get a list of unique 'Genome ID's for each
 g = df.groupby('Genome Name')['Genome ID'].apply(lambda x: list(np.unique(x)))
-g
+
+print(g)
+
+g.to_csv('grouped_genome_ids.csv')
